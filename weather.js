47018ee64e5a1b42f4e77ab805dc4f4164a9b639
m-mulhall <mariarose.mulhall@edu.sait.ca>
2025-04-16 12:20:07 -0600

    update weather.js
    add openweather api for cancun, mexico.

--- weather.js
+++ weather.js
@@ -1,1 +1,8 @@
 
+fetch("https://api.openweathermap.org/data/2.5/weather?q=Cancun,mx&appid=57d3e04d4416d04180e8bdc5dac64198&units=metric")
+.then(response => response.json())
+.then(data => {
+  console.log(data);
+  console.log(data.main.temp);
+  temperature.textContent = "Current Temperature: " + data.main.temp + "\u00b0C";
+})
